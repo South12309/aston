d7652852e37c15b8670770bdf203b465204b47b2
South12309 <south123@mail.ru>
2023-12-11 13:06:05 +0300

    realize add and get methods in MyArrayList

--- src/main/java/aston/takushinov/Main.java
+++ src/main/java/aston/takushinov/Main.java
@@ -1,10 +1,11 @@
 package aston.takushinov;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List
+
         System.out.println("Hello world!");
     }
 }
--- src/main/java/aston/takushinov/MyArrayList.java
+++ src/main/java/aston/takushinov/MyArrayList.java
@@ -1,5 +1,46 @@
 package aston.takushinov;
 
-public class MyArrayList {
+public class MyArrayList<T> implements MyList<T>{
+    private int capacity;
+    private T[] elements;
+    private int size;
 
+    public MyArrayList() {
+        this.capacity = 10;
+    }
+
+    public MyArrayList(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public boolean add(T element) {
+        if (size>= elements.length) {
+           int newSize = size * 2;
+            T[] newElements = (T[]) new Object[newSize];
+            for (int i = 0; i <elements.length; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+        elements[size++] = element;
+        return true;
+    }
+
+    @Override
+    public boolean addAll(MyList<? extends T> element) {
+        return false;
+    }
+
+    @Override
+    public T get(int i) {
+        if (i<size)
+            return elements[i];
+        return null;
+    }
+
+    @Override
+    public boolean remove(T element) {
+        return false;
+    }
 }
