3eb770efadced3340585fe16c332c9ee7e70eb04
Ali Takushinov <south123@mail.ru>
2023-12-20 09:45:12 +0300

    task 3. return numbers with max count of digits in every line (did by comparing length of number)

--- src/main/java/aston/takushinov/io/Main.java
+++ src/main/java/aston/takushinov/io/Main.java
@@ -19,30 +19,30 @@ public class Main {
         Path path = Path.of("resources","text.txt");
         Pattern regPatternForFindWords = Pattern.compile("\\w+", Pattern.UNICODE_CHARACTER_CLASS);
 
-//        //1. Задан файл с текстом, найти и вывести в консоль все слова, начинающиеся с гласной буквы.
-//        String vowels = "аоуэиыеёяю";
-//        try (Stream<String> lines = Files.lines(path)){
-//            lines.flatMap(x->regPatternForFindWords.matcher(x).results())
-//                    .map(MatchResult::group)
-//                    .filter(x->vowels.indexOf(x.charAt(0))!=-1)
-//                    .forEach(System.out::println);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        //2. Задан файл с текстом, найти и вывести в консоль все слова,  для которых последняя буква одного слова совпадает с первой буквой следующего слова
-//        try (Scanner scanner = new Scanner(path)) {
-//            String prev="", curr="";
-//            while (scanner.hasNext()) {
-//                curr = regPatternForFindWords.matcher(scanner.next()).results().findFirst().get().group();
-//                if (!prev.isEmpty() && prev.charAt(prev.length()-1)==curr.charAt(0)) {
-//                    System.out.println(prev + " " + curr);
-//                }
-//                prev = curr;
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
+        //1. Задан файл с текстом, найти и вывести в консоль все слова, начинающиеся с гласной буквы.
+        String vowels = "аоуэиыеёяю";
+        try (Stream<String> lines = Files.lines(path)){
+            lines.flatMap(x->regPatternForFindWords.matcher(x).results())
+                    .map(MatchResult::group)
+                    .filter(x->vowels.indexOf(x.charAt(0))!=-1)
+                    .forEach(System.out::println);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        //2. Задан файл с текстом, найти и вывести в консоль все слова,  для которых последняя буква одного слова совпадает с первой буквой следующего слова
+        try (Scanner scanner = new Scanner(path)) {
+            String prev="", curr="";
+            while (scanner.hasNext()) {
+                curr = regPatternForFindWords.matcher(scanner.next()).results().findFirst().get().group();
+                if (!prev.isEmpty() && prev.charAt(prev.length()-1)==curr.charAt(0)) {
+                    System.out.println(prev + " " + curr);
+                }
+                prev = curr;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
         //3. Задан файл с текстом. В каждой строке найти и вывести наибольшее число цифр, идущих подряд.
         Pattern regPatternForFindNumbers = Pattern.compile("\\d+", Pattern.UNICODE_CHARACTER_CLASS);
@@ -50,8 +50,7 @@ public class Main {
             lines
                     .map(x->regPatternForFindNumbers.matcher(x).results()
                             .map(MatchResult::group)
-                            .map(Integer::valueOf)
-                            .max(Integer::compare))
+                            .max(Comparator.comparingInt(String::length)))
                     .filter(x-> x.isPresent())
                     .map(x->x.get())
                     .forEach(System.out::println);
